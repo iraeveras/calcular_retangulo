198db02540be5636528f56b97d631c836b9bf4f1
Irae Veras <irae.online@gmail.com>
2024-08-13 17:40:20 -0300

    refactor: aula 01 calculo area retangulo

--- aula_01/calcular_area_retangulo.py
+++ aula_01/calcular_area_retangulo.py
@@ -1,10 +1,10 @@
-largura = float(input("Digite a largura: "))
+largura = float(input("Digite a largura da base: "))
 altura = float(input("Digite a altura: "))
 
-resultado = 0
+area = 0
 
 if altura and largura:
-    resultado = largura * altura
-    print(f"A área do retângulo é: {resultado}")
+    area = base * altura
+    print(f"A área do retângulo é: {area}")
 else:
     print("Você precisa informar o valor da largura e altura")
