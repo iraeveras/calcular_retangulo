6e7de60bc2c5188f10909e488c2651a25a2054da
iraeveras <irae.online@gmail.com>
2024-08-23 21:58:25 -0300

    exercicios dicionario

---
+++ modulo_2/aula_02/dicionarios/exercicios.py
@@ -0,0 +1,17 @@
+# ATIVIDADE PRÁTICA 10
+# chave1 = input("Digite a chave: ")
+# valor1 = input("Digite o valor da chave: ")
+
+# dados_pessoais = {f"{chave1}: {valor1}"}
+
+# print(dados_pessoais)
+
+# ATIVIDADE PRÁTICA 11
+# Escreva um programa que recebe um dicionário e uma
+# lista de chaves como entrada e verifica se todas as
+# chaves da lista existem no dicionário. A função deve
+# retornar True se todas as chaves existirem e False caso
+
+
+dicionario = input("Digite um nome para o seu dicionário: ")
+chave = input("Digite um nome para a sua chave: ")
