480f7eeb298bd022453605018fa1ffe18c956dfe
Irae Veras <irae.online@gmail.com>
2024-09-13 13:14:46 -0300

    atividades

--- aula_0609_funcoes/funcao.py
+++ aula_0609_funcoes/funcao.py
@@ -1,11 +1,19 @@
 # ATIVIDADE 1
+'''
+Crie uma função que receba um nome e imprima uma
+saudação personalizada.
+'''
 def saudacao(nome):
     print(f"olá, {nome}")
 
-# saudacao("Julia")
+saudacao("Julia")
 
 
 # ATIVIDADE 2
+'''
+Crie uma função que receba um horário e imprima
+"Bom dia!", "Boa tarde!" ou "Boa noite!" conforme o horário.
+'''
 def comprimento(horario):
     if horario < 12:
         print(f"Bom dia")
@@ -19,6 +27,10 @@ comprimento(horario)
 
 
 # ATIVIDADE 3
+'''
+Crie uma função que receba dois números e retorne a
+soma deles.
+'''
 def soma(num1, num2):
     return f"Soma: {num1 + num2}"
 
@@ -26,11 +38,21 @@ print(soma(12,5))
 
 
 # ATIVIDADE 4
+'''
+Crie uma função que receba dois números e retorne a
+subtração do primeiro pelo segundo.
+'''
 def subtracao(num1, num2):
     return f"Subtração: {num2 - num1}"
 
 print(subtracao(7,2))
 
+
+# ATIVIDADE 5
+'''
+Crie uma função que receba dois números e retorne a
+multiplicação deles.
+'''
 def multiplicacao(num1, num2):
     return f"Multiplicação: {num1 * num2}"
 
