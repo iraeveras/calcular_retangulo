ba788bab19982e58087b4f86a463dafd26d112ee
Irae Veras <irae.online@gmail.com>
2024-09-13 15:26:15 -0300

    prova aula 01  modulo 02

---
+++ modulo_2/prova_aula_01/prova_aula_01.py
@@ -0,0 +1,10 @@
+'''
+Crie uma lista contendo seis frutas de sua escolha. Depois de ter a lista pronta, converta essa lista em uma tupla. Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+'''
+lista_frutas = ["Laranja", "Pessego", "Maça", "Morango", "Banana", "Pêra"]
+
+tupla_frutas = tuple(lista_frutas)
+
+print(tupla_frutas)
+
+
